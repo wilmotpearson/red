fcbf721548c0552d53480bd5f4ce533f5cf3b974
gnellen <nelle004@rangers.uwp.edu>
2015-02-27 14:59:39 -0600

    Update second_page.php

--- second_page.php
+++ second_page.php
@@ -16,7 +16,32 @@ require_once ('MySqlConn.class.php' );
 			href = "style.css">
 	</head>
 	<body>
-	
+	<div id= "wrapper">
+		<header></header>
+			<nav>
+			   <ul>
+				<li><a href="#" >Checklist</a></li>
+				<ul>
+					<li><a>Add Checklist</a></li>
+					<li><a>Edit Checklist</a></li>
+				</ul>
+				<li><a href="#">>Push Notification</a></li>
+				
+				<li><a href="#">Seasonal Changes</a></li>
+				<ul>
+					<li><a>Spring</a></li>
+					<li><a>Summer</a></li>
+					<li><a>Fall</a></li>
+					<li><a>Winter</a></li>
+				</ul>
+				<li><a href="#">>View Damage Reports</a></li>
+
+			  </ul>
+			</nav>
+			
+
+		<footer></footer>
+	</div>
 	
 	</body>
 </html>
