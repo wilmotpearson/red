e9a1e87cfcb5cff0f128c36d06d5ea948c5d8638
gnellen <nelle004@rangers.uwp.edu>
2015-02-27 14:02:42 -0600

    Create second_page.php

---
+++ second_page.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once ('Sanitizer.class.php' );
+require_once ('MySqlConn.class.php' );
+
+?>
+
+<!doctype html>
+
+
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title> Ready Wisconsin</title>
+		<link rel = "stylesheet" type = "text/css"
+			href = "style.css">
+	</head>
+	<body>
+	
+	
+	</body>
+</html>
+	
