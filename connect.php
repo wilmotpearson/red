df2a99fd386a2338177547958c9ca794e4d6a781
wilmotpearson <wilmotpearson@users.noreply.github.com>
2015-02-27 16:29:20 -0600

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,22 +1,61 @@
 
 <?php
+//Password and username entered by user.
+$usernameForm = $_POST['username'];
+$passwordForm= $_POST['password'];
+
+
 
 $hostname = "127.0.0.1";
+
 $username = "root";
+
 $password = "";
+
 $database = "testdb";
+
+
 $db = mysqli_connect ($hostname, $username, $password, $database )or die("There is a problem in sector 7G");
 
 
-$insert = "INSERT INTO users(id, username, password)
-VALUES('2','joe', 'blue')";
 
-$result = mysqli_query($db, $insert);
 
+
+$select = "SELECT u.username, u.password FROM users u WHERE 
+ u.username = '$usernameForm' AND u.password = '$passwordForm';";
+
+
+
+$result = mysqli_query($db, $select);
+echo $select;
 if($result) {
-echo "Sucess";
+
+$rows = mysqli_num_rows($result);
+
+if($rows == 1) {
+
+
+//Redirect from login to county page.
+header("Location: sucess.html");
+
+
+} else {
+echo "Login failed";
+  }
+
+}else {
+header("Location: register.php");
 }
 
 
 
+
+
+
+
+
 ?>
+
+
+
+
